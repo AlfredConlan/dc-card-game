3d3968bee39fc5a321bf753ab8c5d3fe0afac4bf
AlfredConlan <oconellan@gmail.com>
2021-07-29 00:04:35 -0500

    everything is working but after first matched pair execution ends

--- cards.js
+++ cards.js
@@ -1,5 +1,44 @@
-// Browser event for clicking on image
-function imageflip(clicked_id) {
-  // Set the image to the "flipped" state
-  document.getElementById(clicked_id).src = "./card-images/blue_back.jpg";
+let cardOneId = "";
+let cardTwoId = "";
+let numOfCards = 0;
+
+function getCardId(clicked_id) {
+  if (numOfCards == 0) {
+    console.log("Number of cards is: " + numOfCards);
+    cardOneId = clicked_id;
+    cardOneValue = document.getElementById(clicked_id).src;
+    numOfCards = 1;
+    document.getElementById(clicked_id).style =
+      "border-width: 10px; border-style: solid; border-color: green;";
+  } else if (numOfCards == 1) {
+    console.log("Number of cards is: " + numOfCards);
+    cardTwoId = clicked_id;
+    cardTwoValue = document.getElementById(clicked_id).src;
+    numOfCards = 3;
+    document.getElementById(clicked_id).style =
+      "border-width: 10px; border-style: solid; border-color: green;";
+    checkForMatch(cardOneValue, cardTwoValue);
+  } else {
+    // Do nothing for now
+  }
+}
+
+function checkForMatch(cardOne, cardTwo) {
+  if (cardOne == cardTwo) {
+    document.getElementById(cardOneId).style = "visibility: hidden;";
+    document.getElementById(cardTwoId).style = "visibility: hidden;";
+  } else {
+    resetPair();
+  }
+}
+
+function resetPair() {
+  document.getElementById(cardOneId).style =
+    "border-width: 0px; padding: 20px;";
+  document.getElementById(cardTwoId).style =
+    "border-width: 0px; padding: 20px;";
+
+  cardOneId = "";
+  cardTwoId = "";
+  numOfCards = 0;
 }
