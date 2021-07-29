c98d4502011e78830ff2fabea4c0fe5541fce158
AlfredConlan <oconellan@gmail.com>
2021-07-28 19:45:06 -0500

    got card to flip

---
+++ cards.js
@@ -0,0 +1,5 @@
+// Browser event for clicking on image
+function imageflip(clicked_id) {
+  // Set the image to the "flipped" state
+  document.getElementById(clicked_id).src = "./card-images/blue_back.jpg";
+}
